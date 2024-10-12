7c570bdd7e6287c0f10e068e1faaf1bb66f5dd6b
Virat Srivastava <vs.viratsrivastava11111@gmail.com>
2024-10-12 23:39:12 +0530

    Update app.py

--- deployment-backend/app.py
+++ deployment-backend/app.py
@@ -1,17 +1,5 @@
-# File: app.py
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from ml_model import predict
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/predict', methods=['POST'])
-def api_predict():
-    data = request.json
-    result = predict(data['input'])
-    return jsonify({'result': result})
+# app.py
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
