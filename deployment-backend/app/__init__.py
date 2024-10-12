b91b1b06996957e842a3bc2e26e075c8372279f4
Virat Srivastava <vs.viratsrivastava11111@gmail.com>
2024-10-12 23:39:19 +0530

    Update __init__.py

--- deployment-backend/app/__init__.py
+++ deployment-backend/app/__init__.py
@@ -1,14 +1,10 @@
-# File: app/__init__.py
+# app/__init__.py
 from flask import Flask
 from flask_cors import CORS
-from app.config import Config  # Updated import statement
+from app.config import Config
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-    CORS(app)
+app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)
 
-    from app.routes import main
-    app.register_blueprint(main)
-
-    return app
+from app import routes
